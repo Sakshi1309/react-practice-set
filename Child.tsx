482a28f27e28f6f1835337ebe1a7d72d2bc5470b
Sakshi1309 <31957290+Sakshi1309@users.noreply.github.com>
2022-12-27 12:45:56 +0530

    1 file modified

--- Child.tsx
+++ Child.tsx
@@ -25,24 +25,30 @@ export default class Child extends Component {
     // });
   }
 
-  constructor() {
-    super();
-    // this.state = {
-    //   fName: '',
-    //   lName: '',
-    //   city: '',
-    //   userState: '',
-    // };
+  constructor(props) {
+    super(props);
+    this.state = {
+      fName: this.props.fName,
+      lName: this.props.lName,
+      city: this.props.city,
+      userState: this.props.userState,
+    };
   }
 
   render() {
     return (
       <div>
-        <h4>This is a child component </h4>
+        <h4>This is a child component values coming from Props </h4>
         <p>First Name: {this.props.fName} </p>
         <p>Last Name: {this.props.lName} </p>
         <p>City: {this.props.city}</p>
         <p>State: {this.props.userState} </p>
+
+        <h4>This is a child component values coming from States </h4>
+        <p>First Name: {this.state.fName} </p>
+        <p>Last Name: {this.state.lName} </p>
+        <p>City: {this.state.city}</p>
+        <p>State: {this.state.userState} </p>
       </div>
     );
   }
